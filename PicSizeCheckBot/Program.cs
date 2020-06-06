8e47f22308da723c5a9ff45c00539829d07c5466
TehGM <wojop95@gmail.com>
2020-06-06 19:18:07 +0100

    Project init

--- PicSizeCheckBot/Program.cs
+++ PicSizeCheckBot/Program.cs
@@ -1,13 +1,81 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Threading.Tasks;
+using TehGM.WolfBots.PicSizeCheckBot.Options;
 using TehGM.Wolfringo.Hosting;
 
-namespace TehGM.Wolf.PicSizeCheckBot
+namespace TehGM.WolfBots.PicSizeCheckBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            EnableUnhandledExceptionLogging();
+
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("appsecrets.json", optional: true);
+                    config.AddJsonFile($"appsecrets.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    // configure and add hosted wolf client
+                    services.Configure<HostedWolfClientOptions>(context.Configuration.GetSection("WolfClient"));
+                    services.AddWolfClient();
+                    services.AddHostedService<TempMessageHandler>();
+                })
+                .UseSerilog((context, config) => ConfigureSerilog(context, config), true)
+                .Build();
+            await host.RunAsync();
+        }
+
+        private static void ConfigureSerilog(HostBuilderContext context, LoggerConfiguration config)
+        {
+            DatadogOptions ddOptions = context.Configuration.GetSection("Serilog").GetSection("DataDog").Get<DatadogOptions>();
+            config.ReadFrom.Configuration(context.Configuration)
+                .WriteTo.DatadogLogs(
+                    ddOptions.ApiKey,
+                    source: ".NET",
+                    service: ddOptions.ServiceName ?? "WolfBots-Size",
+                    host: ddOptions.HostName ?? Environment.MachineName,
+                    new string[] {
+                                $"env:{(ddOptions.EnvironmentName ?? context.HostingEnvironment.EnvironmentName)}",
+                                $"assembly:{(ddOptions.AssemblyName ?? context.HostingEnvironment.ApplicationName)}"
+                    },
+                    ddOptions.ToDatadogConfiguration(),
+                    // no need for debug logs in datadag
+                    logLevel: ddOptions.OverrideLogLevel ?? LogEventLevel.Verbose
+                );
+        }
+
+        private static void EnableUnhandledExceptionLogging()
+        {
+            // add default logger for errors that happen before host runs
+            Log.Logger = new LoggerConfiguration()
+                        .WriteTo.Console()
+                        .WriteTo.File("data/logs/tepn-unhandled.log",
+                        fileSizeLimitBytes: 1048576,        // 1MB
+                        rollOnFileSizeLimit: true,
+                        retainedFileCountLimit: 5,
+                        rollingInterval: RollingInterval.Day)
+                        .CreateLogger();
+            // capture unhandled exceptions
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Log.Logger.Error((Exception)e.ExceptionObject, "An exception was unhandled");
+                Log.CloseAndFlush();
+            }
+            catch { }
         }
     }
 }
---
+++ PicSizeCheckBot/TempMessageHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TehGM.Wolfringo;
+using TehGM.Wolfringo.Hosting;
+using TehGM.Wolfringo.Messages;
+
+namespace TehGM.WolfBots.PicSizeCheckBot
+{
+    public class TempMessageHandler : IHostedService
+    {
+        private readonly IHostedWolfClient _client;
+
+        public TempMessageHandler(IHostedWolfClient client)
+        {
+            this._client = client;
+            this._client.AddMessageListener<ChatMessage>(OnChatMessage);
+        }
+
+        private async void OnChatMessage(ChatMessage message)
+        {
+            if (message.IsPrivateMessage && message.IsText && message.Text.StartsWith("hello", StringComparison.OrdinalIgnoreCase))
+                await _client.RespondWithTextAsync(message, "Hello there!").ConfigureAwait(false);
+        }
+
+        // Implementing IHostedService ensures this class is created on start
+        Task IHostedService.StartAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+        Task IHostedService.StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
