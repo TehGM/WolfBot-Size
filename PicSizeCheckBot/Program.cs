2ecc32e1a6a5b26986051afb1159db5cc7b5d978
TehGM <wojop95@gmail.com>
2020-06-06 17:36:25 +0100

    Add project files.

---
+++ PicSizeCheckBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TehGM.Wolfringo.Hosting;
+
+namespace TehGM.Wolf.PicSizeCheckBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
