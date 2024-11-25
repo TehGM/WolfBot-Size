ffe7839ad295c07130dddec0d1ffbb5b59b944eb
TehGM <TehGM@users.noreply.github.com>
2024-11-25 13:54:45 +0100

    Fix incorrect library in help message

--- PicSizeCheckBot/HelpHandler.cs
+++ PicSizeCheckBot/HelpHandler.cs
@@ -22,7 +22,7 @@ Bot features and commands: https://github.com/TehGM/WolfBot-Size/wiki
 Questions, suggestions or bugs reports: https://github.com/TehGM/WolfBot-Size/issues.
 Sponsor my work: https://github.com/sponsors/TehGM
 
-Using Wolfringo library v2.1.1
+Using Wolfringo library v2.1.2
 Bot version: v{GetVersion()}
 Copyright © 2020 TehGM",  // due to AGPL licensing, this line cannot be changed or removed, unless by the original author
 cancellationToken);
